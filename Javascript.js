576581f5537d69fe1656cc5c87135253a349e5ac
donskilful <donskilful@gmail.com>
2021-06-29 02:46:35 +0100

    Added some javascript functionalities

---
+++ Javascript.js
@@ -0,0 +1,18 @@
+// // An array of objects
+// var socialMedia = [
+//   { facebook: "Harry" },
+//   { instagram: "Alice" },
+//   { linkedin: "Peter" },
+// ];
+
+// if (Array.isArray(socialMedia)) {
+//   console.log("This is an Array");
+//   // Find if the array contains an object by comparing the property value
+//   if (socialMedia.some((media) => media.facebook === "Harry")) {
+//     console.log("Object found inside the array.");
+//   } else {
+//     console.log("Object not found.");
+//   }
+// } else {
+//   console.log("Cannot perform this operation on a non Array");
+// }
